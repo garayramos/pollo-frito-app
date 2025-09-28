c737f53db34be808f98e7c78a03ce556ddc761ef
Garay <charlygarayramos15@gmail.com>
2025-09-28 15:37:45 -0600

    actualizacion

--- routes/inventarioRoutes.js
+++ routes/inventarioRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { agregarStock } = require('./controllers/inventarioController');
+const { agregarStock } = require('../controllers/inventarioController');
 
 // Solo pones la parte relativa a la ruta
 router.post('/agregar', agregarStock);
